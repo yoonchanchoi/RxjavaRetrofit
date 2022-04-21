719f2bc40ffac7fff5c554e4320b0d22bff065fc
ChoiYoonChan <choyc9239@gmail.com>
2022-04-21 17:45:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/rxjavaretrofit/CovidService.kt
@@ -0,0 +1,11 @@
+package com.example.mvvmretrofit
+
+import io.reactivex.Single
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CovidService{
+    @GET("/korea/country/new/")
+    fun getDocument(@Query("serviceKey") serviceKey: String): Single<StateVO>
+}
